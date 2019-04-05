00b5fedf3decc9c432a3c4882fd1b219f93d8b0e
unknown <rachelker@uchicago.edu>
2019-04-05 13:20:43 -0500

    Problem 1

--- HW1/crime.py
+++ HW1/crime.py
@@ -7,6 +7,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+# Problem 1: Data Acquistion and Analysis
+
 def get_data(csv2017, csv2018):
     '''
     Function to read data from csv and append into one dataframe
@@ -44,21 +46,19 @@ def crimes_by_type(df, groupby_cols):
     return data
 
 
-def get_table_and_charts(data, groupby_col):
+def get_table(data, groupby_col):
     '''
-    Prints a table of crime by type and year and
-    returns a plot (matplotlib.axes.Axes)
+    Returns a table of crime by type and year
 
     Inputs:
         pandas dataframe
-        groupby_cols: list of 2 cols to groupby
+        groupby_cols: list of cols to groupby
     '''
     types_year = crimes_by_type(data, groupby_col)
     sortby = data[groupby_col[1]].max()
-    df = types_year.pivot(index=groupby_col[0], columns='Year', values='Count').sort_values(by=2017, ascending=False)
+    df = types_year.pivot(index=groupby_col[0], columns='Year', values='Count').sort_values(by=2018, ascending=False)
     df_10 = df.iloc[0:10]
-    print(df_10)
-    return df_10.plot.bar(figsize=(20,10), rot=0)
+    return df_10
 
 
 def crime_summary():
@@ -73,29 +73,49 @@ def crime_summary():
     print()
 
     print("Top 10 Reported crimes by type by year")
-    fig = get_table_and_charts(data, ['Primary Type', 'Year'])
+    table1 = get_table(data, ['Primary Type', 'Year'])
+    print(table1)
+    table1.plot.bar(figsize=(20,10), rot=0)
     plt.title('Theft is the most reported crime in Chicago in 2017 and 2018')
     plt.savefig('Top 10 Crime reported by type by year')
     print()
     
     print("Top 10 Arrested crimes by type by year")
-    fig = get_table_and_charts(arrest_data(data), ['Primary Type', 'Year'])
+    table2 = get_table(arrest_data(data), ['Primary Type', 'Year'])
+    print(table2)
+    table2.plot.bar(figsize=(20,10), rot=0)
     plt.title('Narcotics results in an arrest outcome the most in Chicago in 2017 and 2018')
     plt.savefig('Top 10 Crime arrested by type by year')
     print()
 
-    print("Top 10 Neighborhoods with reported crime by year")
-    fig = get_table_and_charts(data, ['Community Area', 'Year'])
-    plt.title('Community Area 25, 8, 32 has the most crime reported in Chicago in 2017 and 2018')
-    plt.savefig("Top 10 Neighborhoods with reported crime by year")
+    print("Top 10 Neighborhoods with most reported crime by year")
+    table3 = get_table(data, ['Community Area', 'Year'])
+    table3.plot.bar(figsize=(20,10), rot=0)
+    plt.title('Community Area 25, 8, 32 has the most reported crime in Chicago in 2017 and 2018')
+    plt.savefig('Top 10 neighborhoods with most reported crime by year')
     print()
     
-    print("Top 10 Neighborhoods with arrests by year")
-    fig = get_table_and_charts(arrest_data(data), ['Community Area', 'Year'])
-    plt.title('Community Area 25, 29, 23 has the most crime arrests in Chicago in 2017 and 2018')
-    plt.savefig("Top 10 Neighborhoods with reported crime by year")
+    print("Top 10 Neighborhoods with most arrests by year")
+    table4 = get_table(arrest_data(data), ['Community Area', 'Year'])
+    table4.plot.bar(figsize=(20,10), rot=0)
+    plt.title('Community Area 25, 29, 23 has the most arrests of reported crime in Chicago in 2017 and 2018')
+    plt.savefig('Top 10 neighborhoods with most arrests by year')
     print()
 
-    # Find out more about these communities (what type of neighborhoods?)
+    print("Top 10 Wards with most reported crime by year")
+    table3 = get_table(data, ['Ward', 'Year'])
+    table3.plot.bar(figsize=(20,10), rot=0)
+    plt.title('Wards 42, 24, 28 has the most reported crime in Chicago in 2017 and 2018')
+    plt.savefig('Top 10 wards with most reported crime by year')
+    print()
+    
+    print("Top 10 Wards with most arrests by year")
+    table4 = get_table(arrest_data(data), ['Ward', 'Year'])
+    table4.plot.bar(figsize=(20,10), rot=0)
+    plt.title('Wards 24, 28, 27 has the most arrests of reported crime in Chicago in 2017 and 2018')
+    plt.savefig('Top 10 wards with most arrests by year')
+    print()
 
+
+# Problem 2:
 
