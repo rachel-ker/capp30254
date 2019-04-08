51d336044e166f1087b4118c48118fc0eb9cfcdf
Rachel Ker <rachelker@cs.uchicago.edu>
2019-04-07 21:17:43 -0500

    edited

--- HW1/crime.py
+++ HW1/crime.py
@@ -6,6 +6,7 @@ Rachel Ker
 import pandas as pd
 import matplotlib.pyplot as plt
 import requests
+import censusgeocode as cg
 
 
 def get_data(url):
@@ -33,6 +34,11 @@ def get_crime_data(year):
     df = get_data(url)
     return df
 
+def get_both_years():
+    data_2017 = get_crime_data(2017)
+    data_2018 = get_crime_data(2018)
+    data = data_2017.append(data_2018)
+    return data
 
 def arrest_data(df):
     '''
@@ -77,17 +83,15 @@ def crime_summary():
     '''
     Gets summary statistics of crime data
     '''
-    data_2017 = get_crime_data(2017)
-    data_2018 = get_crime_data(2018)
-    data = data_2017.append(data_2018)
+    data = get_both_years()
 
     print("Number of reported incidents of crime by year")
-    num_reported = data.groupby('Year').size().to_frame('Count').reset_index()
+    num_reported = data.groupby('year').size().to_frame('Count').reset_index()
     print(num_reported)
     print()
 
     print("Top 10 Reported crimes by type by year")
-    table1 = get_table(data, ['Primary Type', 'Year'])
+    table1 = get_table(data, ['primary_type', 'year'])
     print(table1)
     table1.plot.bar(figsize=(20,10), rot=0)
     plt.title('Theft is the most reported crime in Chicago in 2017 and 2018')
@@ -95,7 +99,7 @@ def crime_summary():
     print()
     
     print("Top 10 Arrested crimes by type by year")
-    table2 = get_table(arrest_data(data), ['Primary Type', 'Year'])
+    table2 = get_table(arrest_data(data), ['primary_type', 'year'])
     print(table2)
     table2.plot.bar(figsize=(20,10), rot=0)
     plt.title('Narcotics results in an arrest outcome the most in Chicago in 2017 and 2018')
@@ -103,28 +107,28 @@ def crime_summary():
     print()
 
     print("Top 10 Neighborhoods with most reported crime by year")
-    table3 = get_table(data, ['Community Area', 'Year'])
+    table3 = get_table(data, ['community_area', 'year'])
     table3.plot.bar(figsize=(20,10), rot=0)
     plt.title('Community Area 25, 8, 32 has the most reported crime in Chicago in 2017 and 2018')
     plt.savefig('Top 10 neighborhoods with most reported crime by year')
     print()
     
     print("Top 10 Neighborhoods with most arrests by year")
-    table4 = get_table(arrest_data(data), ['Community Area', 'Year'])
+    table4 = get_table(arrest_data(data), ['community_area', 'year'])
     table4.plot.bar(figsize=(20,10), rot=0)
     plt.title('Community Area 25, 29, 23 has the most arrests of reported crime in Chicago in 2017 and 2018')
     plt.savefig('Top 10 neighborhoods with most arrests by year')
     print()
 
     print("Top 10 Wards with most reported crime by year")
-    table3 = get_table(data, ['Ward', 'Year'])
+    table3 = get_table(data, ['ward', 'year'])
     table3.plot.bar(figsize=(20,10), rot=0)
     plt.title('Wards 42, 24, 28 has the most reported crime in Chicago in 2017 and 2018')
     plt.savefig('Top 10 wards with most reported crime by year')
     print()
     
     print("Top 10 Wards with most arrests by year")
-    table4 = get_table(arrest_data(data), ['Ward', 'Year'])
+    table4 = get_table(arrest_data(data), ['ward', 'year'])
     table4.plot.bar(figsize=(20,10), rot=0)
     plt.title('Wards 24, 28, 27 has the most arrests of reported crime in Chicago in 2017 and 2018')
     plt.savefig('Top 10 wards with most arrests by year')
@@ -134,8 +138,6 @@ def crime_summary():
 
 # Problem 2: Data Augmentation and APIS
 
-import censusgeocode as cg
-
 def get_fipscode(row):
     '''
     Get census block from latitude and longitude
@@ -143,7 +145,7 @@ def get_fipscode(row):
     Inputs: lat, lon
     Returns census block
     '''
-    result= cg.coordinates(row['Longitude'], row['Latitude'])
+    result= cg.coordinates(row['longitude'], row['latitude'])
     fipscode = result['2010 Census Blocks'][0]['GEOID']
     return fipscode
 
@@ -155,10 +157,10 @@ def adding_fipscode_to_df(df):
     Inputs: Pandas dataframe
     Returns a dataframe with the corresponding zipcode added
     '''
-    df = df[~df['Latitude'].isna() & ~df['Longitude'].isna()]
+    df = df[~df['latitude'].isna() & ~df['longitude'].isna()]
     fips = df.apply(get_fipscode, axis=1)
 
-    df['FIPS Code'] = fips
+    df['fips_code'] = fips
     return df
 
 
@@ -173,9 +175,12 @@ def augment():
     '''
     Augments crime data with ACS data
     '''
-    df = get_data("data/Crimes2017.csv", "data/Crimes2018.csv")
+    df = get_both_years()
 
-    df_homicide = df[df['Primary Type'] == 'HOMICIDE']
+    df_homicide = df[df['primary_type'] == 'BATTERY']
+    df = adding_fipscode_to_df(df_homicide)
+
+    df_homicide = df[df['primary_type'] == 'HOMICIDE']
     df = adding_fipscode_to_df(df_homicide)
     return df
 
