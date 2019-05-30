4be3f33387548487f9c127547cf342e70ba75a0d
unknown <rachelker@uchicago.edu>
2019-05-30 00:39:36 -0500

    updated grid

--- finalpipeline/config.py
+++ finalpipeline/config.py
@@ -88,25 +88,18 @@ def define_clfs_params(grid_size):
             }
        
     test_grid = {
-    'RF':   {'n_estimators': [5], 'max_depth': [5], 'max_features': ['sqrt'], 'min_samples_split': [10], 'n_jobs': [-1], 'random_state': [SEED]},
-    'ET':   {'n_estimators': [5], 'criterion' : ['gini'] ,'max_depth': [5],
+    'RF':   {'n_estimators': [100], 'max_depth': [5], 'max_features': ['sqrt'], 'min_samples_split': [10], 'n_jobs': [-1], 'random_state': [SEED]},
+    'ET':   {'n_estimators': [100], 'criterion' : ['gini'] ,'max_depth': [5],
              'max_features': ['sqrt'],'min_samples_split': [10], 'n_jobs': [-1], 'random_state': [SEED]},
     'AB':   {'algorithm': ['SAMME.R'], 'n_estimators': [5], 'random_state': [SEED]},
     'GB':   {'n_estimators': [5], 'learning_rate' : [0.1],'subsample' : [0.5], 'max_depth': [5], 'random_state': [SEED]},
-    'KNN':  {'n_neighbors': [5],'weights': ['uniform'],'algorithm': ['auto']},
+    'KNN':  {'n_neighbors': [1],'weights': ['uniform'],'algorithm': ['auto']},
     'DT':   {'criterion': ['gini'], 'max_depth': [5], 'min_samples_split': [10], 'random_state': [SEED]},
     'SVM':  {'C' :[10], 'random_state': [SEED]},
     'LR':   {'penalty': ['l1'], 'C': [10], 'random_state': [SEED]},
     'BAG':  {'n_estimators': [1], 'n_jobs': [-1], 'random_state': [SEED]},
     'NB':   {'alpha': [1], 'fit_prior': [True, False]}    
             }
-    
-    tiny_grid = {
-    'RF':   {'n_estimators': [100], 'max_depth': [10], 'max_features': ['sqrt'],
-             'min_samples_split': [10], 'n_jobs': [-1], 'random_state': [SEED]},
-    'DT':   {'criterion': ['gini', 'entropy'], 'max_depth': [1,5,10,20], 'min_samples_split': [2,10,50], 'random_state': [SEED]},
-    'LR':   {'penalty': ['l2'], 'C': [0.01,0.1,1], 'random_state': [SEED]}
-               }
 
     
     if (grid_size == 'large'):
@@ -115,7 +108,5 @@ def define_clfs_params(grid_size):
         return small_grid
     elif (grid_size == 'test'):
         return test_grid
-    elif (grid_size == 'tiny'):
-        return tiny_grid
     else:
         return 0, 0
